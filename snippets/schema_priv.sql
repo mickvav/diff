2840105c400db12c912751100ebbc3e56fa39723
Michael Vasilyev <mikhail.vasilyev@workday.com>
2023-10-11 17:25:20 +0100

    schema capitalised, views are tables for GRANT.

--- snippets/schema_priv.sql
+++ snippets/schema_priv.sql
@@ -4,16 +4,16 @@ select nspname,
 object_type,
 (string_to_array(rtrim(ltrim(aclexplode(nspacl)::text,'('),')'),',')) as perm_string
 from  (select nspname, 
-  'schema' as object_type,
+  'SCHEMA' as object_type,
   nspacl from pg_namespace 
    where nspname not like 'pg_%' and nspname not in ('public', 'information_schema')
  union
  select nspname, 
   case(defaclobjtype)
-	  when 'S' then 'sequence'
-      when 'r' then 'table'
-      when 'm' then 'mview'
-      when 'v' then 'view'
+	  when 'S' then 'SEQUENCE'
+      when 'r' then 'TABLE'
+      when 'm' then 'TABLE'
+      when 'v' then 'TABLE'
       else 'other'
       end,
   d.defaclacl from pg_default_acl d
